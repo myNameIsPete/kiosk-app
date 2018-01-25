c38e5c6c6102c90cb5cbd7eaaef293b95d118c9f
petelupiano <pete@digitalfingerprint.ca>
2018-01-25 18:56:45 -0500

    More endpoints added...need to tighten it up though

--- modules/db.js
+++ modules/db.js
@@ -1,6 +1,6 @@
 const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
-const _orderBy = require('lodash').orderBy;
+const async = require('async');
+const groupArray = require('group-array');
 
 // Connection URL
 const url = 'mongodb://localhost:27017';
@@ -8,20 +8,60 @@ const url = 'mongodb://localhost:27017';
 // Database Name
 const dbName = 'BASHR';
 
-module.exports = {
-  getData: function(request, response, query, collectionName) {
-    MongoClient.connect(url, (err, client) => {
-      const db = client.db(dbName);
-      const collection = db.collection(collectionName);
-      collection.find().toArray((err, result) => {
-        const results =
-          result.length === 0 ? { message: 'No results found.' } : result;
-        // results = _orderBy(results, ['year'], ['desc']);
+function getData(query, collectionName, callback) {
+  query = query || {};
+  MongoClient.connect(url, (err, client) => {
+    const db = client.db(dbName);
+    const collection = db.collection(collectionName);
 
-        response.send(results);
-      });
-
-      client.close();
+    collection.find(query).toArray((err, result) => {
+      const members =
+        result.length === 0 ? { message: 'No results found.' } : result;
+      callback(null, members);
     });
+
+    client.close();
+  });
+}
+
+function orderByYear(data, callback) {
+  const members = groupArray(data, 'year');
+  callback(null, members);
+}
+
+module.exports = {
+  getAllMembers: function(query, collectionName, response) {
+    async.waterfall(
+      [async.apply(getData, query, collectionName)],
+      (err, result) => response.send({ result })
+    );
+  },
+
+  getMembersGroupedByYear: function(query, collectionName, response) {
+    async.waterfall(
+      [async.apply(getData, query, collectionName), orderByYear],
+      (err, result) => response.send({ result })
+    );
+  },
+
+  getMembersByType: function(query, collectionName, response) {
+    async.waterfall(
+      [async.apply(getData, query, collectionName)],
+      (err, result) => response.send({ result })
+    );
+  },
+
+  getMembersBySport: function(query, collectionName, response) {
+    async.waterfall(
+      [async.apply(getData, query, collectionName)],
+      (err, result) => response.send({ result })
+    );
+  },
+
+  getMembersBySportGroupedByYear: function(query, collectionName, response) {
+    async.waterfall(
+      [async.apply(getData, query, collectionName), orderByYear],
+      (err, result) => response.send({ result })
+    );
   }
 };
--- server.js
+++ server.js
@@ -7,16 +7,37 @@ app.use(express.static('./public'));
 // ==| ROUTES |=======================================
 
 // Get All Inductees
-app.get('/api/members', (request, response) => {
+app.get('/api/getAllMembers', (request, response) => {
   const query = {};
-  db.getData(request, response, query, 'members');
+  db.getAllMembers(query, 'members', response);
 });
 
-// Get All Inductees & Order by Year
-app.get('/api/members/orderedBy/year', (request, response) => {
+// Get All Inductees & Group By Year
+app.get('/api/getAllMembers/groupBy/year', (request, response) => {
   const query = {};
-  db.getData(request, response, query, 'members');
+  db.getMembersGroupedByYear(query, 'members', response);
 });
+
+// Get All Inductees by Member Type
+app.get('/api/getAllMembers/inMemberType/:member_type', (request, response) => {
+  const query = { member_type: request.params.member_type };
+  db.getMembersByType(query, 'members', response);
+});
+
+// Get All Inductees by Sport
+app.get('/api/getAllMembers/inSport/:sport', (request, response) => {
+  const query = { sport: request.params.sport };
+  db.getMembersBySport(query, 'members', response);
+});
+
+// Get All Inductees by Sport AND Group By Year
+app.get(
+  '/api/getAllMembers/inSport/:sport/groupBy/year',
+  (request, response) => {
+    const query = { sport: request.params.sport };
+    db.getMembersBySportGroupedByYear(query, 'members', response);
+  }
+);
 
 app.listen(3000, () => {
   console.log('Server is running on localhost:3000');
